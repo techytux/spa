c4e3c09a62fd7ac4b7abd25d440e5dd18b5e6a13
Vaibhav Singh <vaibhavs@projectplace.com>
2014-04-23 20:26:59 +0530

    Created API to return projects

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,5 +1,21 @@
 from app import app
+from flask.ext.restful import Api, Resource
+from app.models.models import Project
+from flask import jsonify
+import logging
+from logging.handlers import RotatingFileHandler
+import json, datetime
+from bson import objectid
+api = Api(app)
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+class ProjectCatalog(Resource):
+    def get(self):
+        projects = Project.objects()
+        return jsonify({"projects":projects.to_json()})
+
+api.add_resource(ProjectCatalog, '/projects/')
+
